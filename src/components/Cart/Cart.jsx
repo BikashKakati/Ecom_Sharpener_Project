24fc6267abf2b7f594d42dc4c158fc4330a2ab1b
Bikash Kakati <bikashkakati796@gmail.com>
2024-01-07 12:13:06 +0530

    Feature- Remove cart product
    User can now remove cart product

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -4,31 +4,38 @@ import React from 'react'
 import { useCartContext } from '../../context/CartContext'
 
 function Cart({ openCart, setOpenCart }) {
-    const {cartProductsDetails} = useCartContext();
+    const { cartProductsDetails, setCartProductsDetails } = useCartContext();
+    function handleProductRemove(productId){
+        const updatedCartDetails = cartProductsDetails.filter((product) => product.id !== productId);
+        setCartProductsDetails(updatedCartDetails);
+    }
     return (
         <div className={`${openCart ? 'w-[500px]' : 'w-0'} h-[543px] bg-gray-300 fixed right-0 top-16 z-10 transition-all overflow-y-scroll py-6`}>
             <div className="flex flex-col">
                 <XMarkIcon className="h-6 w-6 my-3 mx-10" strokeWidth={2} onClick={() => setOpenCart(!openCart)} />
                 <div className="w-full p-5">
                     {
-                        cartProductsDetails?.map((cartProducts) =>{
-                            const {name, price, quantity,imageUrl} = cartProducts;
-                            return(
-                                <Card className='w-full mb-5 flex-row' key={cartProducts.id}>
-                                <CardHeader floated={false} className='w-24 m-0'>
-                                    <img src={imageUrl} alt="cart1" />
-                                </CardHeader>
-                                <CardBody className='flex'>
-                                    <div className="mr-5">
-                                        <Typography as="p" variant='h6'>{name}</Typography>
-                                        <Typography as='span' className='text-sm'>Price {price}</Typography>
-                                    </div>
-                                    <Typography as="span" variant='h6'>qty×{quantity}</Typography>
-                                </CardBody>
-                                <CardFooter>
-                                    <Button type="primary">Remove</Button>
-                                </CardFooter>
-                            </Card>
+                        cartProductsDetails?.map((cartProducts) => {
+                            const { id, title, price, quantity, imageUrl } = cartProducts;
+                            return (
+                                <Card className='h-28 w-full mb-5 flex-row items-center' key={id}>
+                                    <CardHeader floated={false} className='w-28 m-0'>
+                                        <img src={imageUrl} alt="cart1" className='w-full object-cover object-center' />
+                                    </CardHeader>
+                                    <CardBody className='flex'>
+                                        <div className="mr-5">
+                                            <Typography as="p">{title}</Typography>
+                                            <Typography as="span" className='text-sm' >Price {price}</Typography>
+                                        </div>
+                                        <Typography as="span" variant='h6'>qty×{quantity}</Typography>
+                                    </CardBody>
+                                    <CardFooter>
+                                        <Button type="primary"
+                                            onClick={() => { handleProductRemove(id) }}>
+                                            Remove
+                                        </Button>
+                                    </CardFooter>
+                                </Card>
                             )
                         })
                     }
